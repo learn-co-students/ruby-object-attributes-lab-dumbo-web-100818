5d82edad9e5d5fd303908b7d65a50592dba5f2c8
github username <github email address>
2018-10-10 19:43:58 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(pername)
+    @name = pername
+  end
+
+  def name
+    @name
+  end
+
+  def job=(jobs)
+    @job = jobs
+  end
+
+  def job
+    @job
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Dog
+  def name=(dogname)
+    @name =dogname
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dogbreed)
+    @breed =dogbreed
+  end
+  def breed
+    @breed
+  end
+end
